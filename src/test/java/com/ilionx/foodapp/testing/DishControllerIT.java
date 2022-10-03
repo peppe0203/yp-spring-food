e9973c0e93d91c88b93238cec2867b90f66a8517
Giuseppe Collura <GCollura@ilionx.com>
2022-10-03 10:52:27 +0200

    added new intergration tests

--- src/test/java/com/ilionx/foodapp/testing/DishControllerIT.java
+++ src/test/java/com/ilionx/foodapp/testing/DishControllerIT.java
@@ -13,6 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.util.ArrayList;
+import java.util.Stack;
 
 
 // This name cannot end with Test because it is an integration test, and we don't want to run this with test.
@@ -27,7 +28,7 @@ public class DishControllerIT {
     private static long currentId = -1;
 
 
-    // The next test is for initialisation the basic database
+    // The next test is for initialisation, for a basic database
     @Test
     @Order(0)
     public void setup() {
@@ -56,9 +57,10 @@ public class DishControllerIT {
     private static class dishList extends ArrayList<Dish> {
     }
 
+    // Create a new dish object and add this to the database
     @Test
     @Order(2)
-    public void testSaving() {
+    public void testCreateDish() {
         Dish newDish = new Dish();
         newDish.setName("Pesto speciale");
         newDish.setGroupName("pasta");
@@ -82,14 +84,63 @@ public class DishControllerIT {
     @Order(3)
     public void testFindById(){
         long id = 1;
-        ResponseEntity<Dish> response = this.restTemplate.getForEntity("/dish/" + 1, Dish.class);
+        ResponseEntity<Dish> response = this.restTemplate.getForEntity("/dish/" + id, Dish.class);
         assertEquals(1, response.getBody().getId());
         assertEquals("Pesto speciale", response.getBody().getName());
     }
 
-    // Count amount of dishes and delete one, count again and this needs to be equal + 1
     @Test
     @Order(4)
+    public void testFindByName(){
+        String dishName = "Pepperoni";
+        ResponseEntity<Dish> response = this.restTemplate.getForEntity("/dish/name:" + dishName, Dish.class);
+        assertNotEquals(0,response.getBody().getId());
+        assertEquals(dishName, response.getBody().getName());
+    }
+
+    @Test
+    @Order(5)
+    public void testFindByGroup(){
+        ResponseEntity<dishList> response = this.restTemplate.getForEntity(
+                "/dish/group:" + "pasta",
+                dishList.class);
+        assertEquals(2, response.getBody().stream().count());
+    }
+
+    @Test
+    @Order(6)
+    public void testFindBetweenPrice(){
+        long startPrice = 5;
+        long endPrice = 12;
+        ResponseEntity<dishList> allDishesBetween = this.restTemplate.getForEntity(
+                "/dish/price/from/" + startPrice + "/to/" + endPrice,
+                dishList.class);
+        assertEquals(1, allDishesBetween.getBody().stream().count());
+    }
+
+    // Update by ID, change the name of a dish
+    @Test
+    @Order(7)
+    public void testUpdateById(){
+        // Collect dish info
+        long id = 1;
+        ResponseEntity<Dish> response = this.restTemplate.getForEntity("/dish/" + id, Dish.class);
+        String oldName = response.getBody().getName();
+
+        // Change dish name
+        response.getBody().setName("Pesto");
+
+        // Collect dish info again to compare
+        this.restTemplate.put("/dish/" + id, response);
+        ResponseEntity<Dish> newResponse = this.restTemplate.getForEntity("/dish/" + id, Dish.class);
+        String newName = response.getBody().getName();
+
+        assertNotEquals(oldName, newName);
+    }
+
+    // Count amount of dishes and delete one, count again and this needs to be equal + 1
+    @Test
+    @Order(8)
     public void testDeleteById(){
         dishList allDishes = this.restTemplate.getForEntity("/dish", dishList.class).getBody();
         long orgAmountDishes = allDishes.stream().count();
